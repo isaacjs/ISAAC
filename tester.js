5884bf41400fb937a5655a245f288915ee26fba6
Jerome Cheng <jcheng31@gmail.com>
2012-01-13 09:58:54 +0800

    Change tester to add console logging for movement module.

--- tester.js
+++ tester.js
@@ -1,10 +1,12 @@
 displayProperties(MovableBall.motion);
 var movementResult1 = movementModule(MovableBall);
 displayProperties(MovableBall.motion);
+console.log(movementResult1);
 console.log("\n");
 
 
 displayProperties(FixedBall.motion);
 var movementResult2 = movementModule(FixedBall);
 displayProperties(FixedBall.motion);
+console.log(movementResult2);
 console.log("\n");
