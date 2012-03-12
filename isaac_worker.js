254b0ceee5ace4dfbecf2e9c615f8e1ff5bebf5e
Jerome Cheng <jcheng31@gmail.com>
2012-03-12 12:29:21 +0800

    Basic working Web Worker implementation. TODO: settings updating.

--- isaac_worker.js
+++ isaac_worker.js
@@ -15,14 +15,32 @@ self.addEventListener('message', function(e) {
 	}
 });
 
-var posQueue = Queue(60);
+// We don't store a buffer of frames - just the latest one.
+// This means that when the program frame rate lowers, we end up
+// dropping frames to keep the simulation rate the same.
+
+// Frame rate should always be less than or equal to update rate.
+var posQueue = Queue(1);
 var config = Config();
 var count = 0;
 var timer;
 
 function timeLoop() {
-	posQueue.enqueue(count);
-	count++;
+	update();
+	posQueue.enqueue({
+		'earthPos' : Earth.motion.position,
+		'sunPos' : Sun.motion.position,
+		'marsPos' : Mars.motion.position,
+		'saturnPos' : Saturn.motion.position,
+		'jupiterPos' : Jupiter.motion.position,
+		'plutoPos' : Pluto.motion.position,
+		'uranusPos' : Uranus.motion.position,
+		'neptunePos' : Neptune.motion.position,
+		'mercuryPos' : Mercury.motion.position,
+		'venusPos' : Venus.motion.position
+	});
+	
+	// Update positions 60 times a second.
 	timer = setTimeout(timeLoop, 50/3);
 }
 
