b3a74b68e71236e29e2522e2c07e958671b4f782
Jerome Cheng <jcheng31@gmail.com>
2012-03-12 11:13:26 +0800

    Start working on new implementation of core logic.

--- isaac_worker.js
+++ isaac_worker.js
@@ -1,12 +1,26 @@
-importScripts('isaac_math.js', 'isaac_obj.js', 'isaac_core.js', 'isaac_planets.js', 'isaac_orbit_phys.js');
+importScripts('isaac_math.js', 'isaac_obj.js', 'isaac_core.js', 'isaac_planets.js', 'isaac_orbit_phys.js', 'isaac_queue');
 
 self.addEventListener('message', function(e) {
-
+	var command = e.data['command'];
+	switch(command) {
+		case "get" :
+			self.postMessage(posQueue.dequeue());
+			break;
+		case "set" :
+			clearTimeout(timer);
+			config = e.data['config'];
+			timeLoop();
+			break;
+	}
 });
+
+var posQueue = Queue(60);
+var config = Config();
+var timer;
 
 function timeLoop() {
 	update();
-	setTimeout(timeLoop(), 1/60);
+	timer = setTimeout(timeLoop(), 1/60);
 }
 
 timeLoop();
