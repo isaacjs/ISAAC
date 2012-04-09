a4f602284b62cc9ba4eeeb15e90507cd850883b7
Jerome Cheng <jcheng31@gmail.com>
2012-04-09 12:30:43 +0800

    Corrected incorrect data for Saturn, corrected direction of orbits.

--- isaac_planets.js
+++ isaac_planets.js
@@ -1,5 +1,6 @@
 // Planet positions and velocities taken from JPL's HORIZONS system,
 // for April 09, 2012, 00:00:00.0000CT.
+// Note that X, Y, Z in Three.js correspond to Y, X, Z in HORIZONS.
 // The Sun.
 var Sun = PhysicalObject();
 Sun.motion.position = [0, 0, 0];
@@ -12,8 +13,8 @@ Sun.switches.accelerationEnabled = true;
 // The Earth.
 var Earth = PhysicalObject();
 Earth.physical.mass = 5.9721986e18;
-Earth.motion.position = [-141.3464397912099, -49.70175623515072, 1.845629910630098e-3];
-Earth.motion.velocity = [9.387467194099132e-6, -2.820546804750964e-5, -3.335489234220996e-10];
+Earth.motion.position = [-49.70175623515072, -141.3464397912099, 1.845629910630098e-3];
+Earth.motion.velocity = [-2.820546804750964e-5, 9.387467194099132e-6, -3.335489234220996e-10];
 Earth.Name = "Earth";
 
 Earth.switches.motionEnabled = true;
@@ -21,9 +22,9 @@ Earth.switches.accelerationEnabled = true;
 
 // Mars.
 var Mars = PhysicalObject();
-Mars.motion.position = [-247.0577964253138, 2.607918972403293, 6.120960274943644];
+Mars.motion.position = [2.607918972403293, -247.0577964253138, 6.120960274943644];
 Mars.physical.mass = 6.5185e17;
-Mars.motion.velocity = [6.507888620275963e-7, -2.216028295316947e-5, -4.802973165012580e-7];
+Mars.motion.velocity = [-2.216028295316947e-5, 6.507888620275963e-7, -4.802973165012580e-7];
 Mars.Name = "Mars";
 
 Mars.switches.motionEnabled = true;
@@ -31,9 +32,9 @@ Mars.switches.accelerationEnabled = true;
 
 // Mercury.
 var Mercury = PhysicalObject();
-Mercury.motion.position = [-34.34126305, -59.83881593, -1.738227286];
+Mercury.motion.position = [-59.83881593, -34.34126305, -1.738227286];
 Mercury.physical.mass = 3.3022e17;
-Mercury.motion.velocity = [3.242607698e-5, -2.190935367e-5, -4.765349366e-6];
+Mercury.motion.velocity = [-2.190935367e-5, 3.242607698e-5, -4.765349366e-6];
 Mercury.Name = "Mercury";
 
 Mercury.switches.motionEnabled = true;
@@ -41,9 +42,9 @@ Mercury.switches.accelerationEnabled = true;
 
 // Jupiter.
 var Jupiter = PhysicalObject();
-Jupiter.motion.position = [480.9176848793924, 571.0765569421500, -13.13349118484296];
+Jupiter.motion.position = [571.0765569421500, 480.9176848793924, -13.13349118484296];
 Jupiter.physical.mass = 1.8988e21;
-Jupiter.motion.velocity = [-1.016103900367553e-5, 9.045517267735690e-6, 1.897925792649849e-7];
+Jupiter.motion.velocity = [9.045517267735690e-6, -1.016103900367553e-5, 1.897925792649849e-7];
 Jupiter.Name = "Jupiter";
 
 Jupiter.switches.motionEnabled = true;
@@ -51,9 +52,9 @@ Jupiter.switches.accelerationEnabled = true;
 
 // Neptune.
 var Neptune = PhysicalObject();
-Neptune.motion.position = [3914.732947116928, -2193.945531342488, -45.02107870865629];
+Neptune.motion.position = [-2193.945531342488, 3914.732947116928, -45.02107870865629];
 Neptune.physical.mass = 1.0278e20;
-Neptune.motion.velocity = [2.613881670696835e-6, 4.780319271610741e-6, -1.586835000701678e-7];
+Neptune.motion.velocity = [4.780319271610741e-6, 2.613881670696835e-6, -1.586835000701678e-7];
 Neptune.Name = "Neptune";
 
 Neptune.switches.motionEnabled = true;
@@ -61,9 +62,9 @@ Neptune.switches.accelerationEnabled = true;
 
 // Venus.
 var Venus = PhysicalObject();
-Venus.motion.position = [-102.3178380016565, 32.62303974857563, 6.352081275507866];
+Venus.motion.position = [32.62303974857563, -102.3178380016565, 6.352081275507866];
 Venus.physical.mass = 4.869e18;
-Venus.motion.velocity = [-1.080589728829473e-5, -3.352572735644723e-5, 1.643058486842552e-7];
+Venus.motion.velocity = [-3.352572735644723e-5, -1.080589728829473e-5, 1.643058486842552e-7];
 Venus.Name = "Venus";
 
 Venus.switches.motionEnabled = true;
@@ -71,9 +72,9 @@ Venus.switches.accelerationEnabled = true;
 
 // Uranus.
 var Uranus = PhysicalObject();
-Uranus.motion.position = [2993.163750777438, 235.9479785280531, -37.89556426125482];
+Uranus.motion.position = [235.9479785280531, 2993.163750777438, -37.89556426125482];
 Uranus.physical.mass = 8.6625e19;
-Uranus.motion.velocity = [-5.930129454709614e-7, 6.477979332815045e-6, 3.178365529507968e-8];
+Uranus.motion.velocity = [6.477979332815045e-6, -5.930129454709614e-7, 3.178365529507968e-8];
 Uranus.Name = "Uranus";
 
 Uranus.switches.motionEnabled = true;
@@ -81,9 +82,9 @@ Uranus.switches.accelerationEnabled = true;
 
 // Saturn.
 var Saturn = PhysicalObject();
-Saturn.motion.position = [-1308.125257452327, -631.8980859148376, 63.06897677571023];
+Saturn.motion.position = [-631.8980859148376, -1308.125257452327, 63.06897677571023];
 Saturn.physical.mass = 5.685e20;
-Saturn.motion.velocity = [-1308.125257452327, -631.8980859148376, 63.06897677571023];
+Saturn.motion.velocity = [-8.711916465865464e-6, 3.671844960056266e-6, 4.785296340228841e-9];
 Saturn.Name = "Saturn";
 
 Saturn.switches.motionEnabled = true;
@@ -91,9 +92,9 @@ Saturn.switches.accelerationEnabled = true;
 
 // Pluto.
 var Pluto = PhysicalObject();
-Pluto.motion.position = [638.1080671989861, -4764.888577948751, 325.4419510356392];
+Pluto.motion.position = [-4764.888577948751, 638.1080671989861, 325.4419510356392];
 Pluto.physical.mass = 1.314e16;
-Pluto.motion.velocity = [5.476987352349563e-6, -3.403729996552767e-7, -1.528481974366561e-6];
+Pluto.motion.velocity = [-3.403729996552767e-7, 5.476987352349563e-6, -1.528481974366561e-6];
 Pluto.Name = "Pluto";
 
 Pluto.switches.motionEnabled = true;
